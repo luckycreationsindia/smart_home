f442e216edf1dd885f568c8e8daf100084862b24
Lokesh Jain <luckycreationsindia@gmail.com>
2023-09-01 19:44:03 +0530

    *Add Animation for Slider

--- lib/src/ui/widgets/light_controller.dart
+++ lib/src/ui/widgets/light_controller.dart
@@ -11,6 +11,7 @@ class LightController extends StatefulWidget {
 
 class _LightControllerState extends State<LightController> {
   double brightness = 0.2;
+  bool onOffState = true;
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +21,28 @@ class _LightControllerState extends State<LightController> {
           child: Row(
             children: [
               Expanded(
-                child: Image.asset(Assets.imagesBulb),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    onOffState ? Container(
+                      height: 2,
+                      width: 2,
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(100)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.yellow.shade300,
+                            blurRadius: brightness * 10,
+                            spreadRadius: brightness * 10,
+                            offset: const Offset(0.0, 0.0),
+                          )
+                        ],
+                      ),
+                    ) : const SizedBox(),
+                    Image.asset(Assets.imagesBulb),
+                  ],
+                ),
               ),
               RotatedBox(
                 quarterTurns: 3,
@@ -29,9 +51,9 @@ class _LightControllerState extends State<LightController> {
                   label: brightness.toString(),
                   max: 1,
                   min: 0,
-                  onChanged: (v) {
+                  onChanged: onOffState ? (v) {
                     setState(() => brightness = v);
-                  },
+                  } : null,
                   onChangeEnd: (v) {},
                   inactiveColor: const Color(0xFFF8F8FA),
                   activeColor: const Color(0xFF05050A),
@@ -40,7 +62,13 @@ class _LightControllerState extends State<LightController> {
             ],
           ),
         ),
-        const LightControllerWidgets(),
+        LightControllerWidgets(
+          onOffChange: (state) {
+            setState(() {
+              onOffState = state;
+            });
+          },
+        ),
       ],
     );
   }
